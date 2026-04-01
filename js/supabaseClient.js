ff5c4615ab2d3bb86a8e0da90a7e3b1b9b3d046a
johnsongomes <32249534+johnsongomes@users.noreply.github.com>
2026-04-01 16:51:05 -0300

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-const supabaseUrl = process.env.SUPABASE_URL
-const supabasekey = process.env.SUPABASE_KEY 
+const supabaseUrl = 'https://neyxzaqxwbhndgzurfna.supabase.co'
+const supabasekey = 'sb_publishable_VSiqjz91xYnkDwC3F-HPtg_Tl0WUhkE'
 export const supabase = createClient(supabaseUrl, supabasekey);

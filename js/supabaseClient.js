4b2be053b5a75afb57f04e99d7c9e7ad9c9a0be7
johnsongomes <32249534+johnsongomes@users.noreply.github.com>
2026-04-01 16:45:59 -0300

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 const supabaseUrl = process.env.SUPABASE_URL
 const supabasekey = process.env.SUPABASE_KEY 
 export const supabase = createClient(supabaseUrl, supabasekey);
